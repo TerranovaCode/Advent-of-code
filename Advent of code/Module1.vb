3d37c88324990d0f5898ca5f10a1d1b6033e510d
david <david@DAVIDSLAPTOP>
2024-12-02 22:42:33 +0000

    Add project files.

---
+++ Advent of code/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Test()
+
+    End Sub
+    Sub Test()
+        Dim ListA As String = Console.ReadLine()
+        Dim ListB As String = Console.ReadLine
+
+
+        Console.WriteLine(ListA)
+    End Sub
+End Module
